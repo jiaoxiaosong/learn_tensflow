4615925cc5352ad71cf8d036907c21a4ea27f043
xiaosongzi <872909437@qq.com>
2019-05-27 13:18:02 +0800

    tensflow 基本运算

---
+++ tensflow_01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/27 上午11:34
+# @Author  : baby松
+# @FileName: tensflow_01.py
+# @Software: PyCharm
+
+import tensorflow as tf
+# constant 常量
+a = tf.constant(10)
+b = tf.constant(5)
+
+with tf.Session() as sess:
+    c = a+b
+    r = sess.run(c)
+    print(r)
